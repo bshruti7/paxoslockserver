1f6cd427b1ee733483eaf041be0e23ce0e59e183
Shruti <bshruti7@uw.edu>
2014-03-07 00:15:24 -0800

    Added Server file (incomplete) to PaxosLockServer project

---
+++ paxoslockserver/PaxosLockServer/src/com/uw/paxos/Server.java
@@ -0,0 +1,20 @@
+package src.com.uw.paxos;
+import java.net.ServerSocket;
+
+/**
+ * 
+ */
+
+/**
+ * @author Shruti
+ *
+ */
+public class Server {
+
+	public static void main(String args[]){
+		
+		
+		
+	}
+	
+}
