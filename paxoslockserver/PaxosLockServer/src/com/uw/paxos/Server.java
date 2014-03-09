7e197dcbed8ef59674b766f6fce6abec17d4f765
Shruti <bshruti7@uw.edu>
2014-03-08 19:42:14 -0800

    Established basic TCp Ip connection. Currently LockClient and LockServer are separate projects by the way( which is the correct method i believe ).

--- paxoslockserver/PaxosLockServer/src/com/uw/paxos/Server.java
+++ paxoslockserver/PaxosLockServer/src/com/uw/paxos/Server.java
@@ -1,6 +1,6 @@
-package src.com.uw.paxos;
-import java.net.ServerSocket;
-
+package com.uw.paxos;
+import java.net.*;
+import java.io.*;
 /**
  * 
  */
@@ -11,10 +11,38 @@ import java.net.ServerSocket;
  */
 public class Server {
 
-	public static void main(String args[]){
+	public static void main(String args[]) throws Exception{
 		
+		
+		Server s1=new Server();
+		s1.go();
 		
 		
 	}
+	
+	
+	public void go() throws Exception{
+		String receivedFromClient;
+					
+		ServerSocket serverSock = new ServerSocket(6000);
+		
+		
+		
+		
+		while(true){
+			Socket sock=serverSock.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			receivedFromClient = inFromClient.readLine();
+			System.out.println("Received this from client: " + receivedFromClient);
+			
+			
+		}
+		
+		
+		//serverSock.close();
+		
+		
+	}
+	
 	
 }
